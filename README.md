c64fd45340d828414eefc4fcc480909df41b656c
Maysson Ferro <mayssonalexandre@gmail.com>
2025-12-15 15:11:31 -0400

    fixes

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-Para rodar o projeto:
+# Para rodar o projeto
+
+Iniciar o gerenciador de módulos do Go
+'''
+go mod init projeto-final
+'''
+
+Baixe as dependencias
+
+'''
+go get -u github.com/gin-gonic/gin
+
+go get -u gorm.io/gorm
+go get github.com/glebarez/sqlite
+'''
 
 '''
 go run main.go
 '''
 
-Acesse no navegador: http://localhost:8080
+Acesse no navegador: <http://localhost:8080>
 você verá o {"message":"Hello, World!"}
 
 o arquivo estoque.db deve ter aparecido
--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// definindo a struct do Produto (e criando a tabela no banco de dados)
 type Produto struct {
 	gorm.Model
 	Nome       string
@@ -18,26 +17,21 @@ type Produto struct {
 }
 
 func main() {
-	// conexão com o banco de dados (se nao existir, cria o arquivo estoque.db)
 	db, err := gorm.Open(sqlite.Open("estoque.db"), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Erro detalhado:", err)
 		panic("falha ao conectar ao banco de dados")
 	}
 
-	// auto migration - cria a tabela Produtos se nao existir
 	db.AutoMigrate(&Produto{})
 
-	// configuração do gin
 	r := gin.Default()
 
-	// rota Hello World
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, World!",
 		})
 	})
 
-	// rodar servidor na porta 8080
 	r.Run(":8080")
 }
